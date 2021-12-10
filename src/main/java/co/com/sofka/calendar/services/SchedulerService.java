cc16de4cfb1d0cf9f72d5afaba366c80c611aa20
juanfvalencia <90592176+juanfvalencia@users.noreply.github.com>
2021-12-10 14:29:18 -0500

    Se realiza return Flux <ProgramDate>

--- src/main/java/co/com/sofka/calendar/services/SchedulerService.java
+++ src/main/java/co/com/sofka/calendar/services/SchedulerService.java
@@ -5,6 +5,7 @@ import co.com.sofka.calendar.model.ProgramDate;
 import co.com.sofka.calendar.repositories.ProgramRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -24,7 +25,7 @@ public class SchedulerService {
     private ProgramRepository programRepository;
 
     //TODO: deben retornar un flux de programDate Flux<ProgramDate>
-    public List<ProgramDate> generateCalendar(String programId, LocalDate startDate) {
+    /*public List<ProgramDate> generateCalendar(String programId, LocalDate startDate) {
         var endDate = new AtomicReference<>(LocalDate.from(startDate));
         final AtomicInteger[] pivot = {new AtomicInteger()};
         final int[] index = {0};
@@ -37,6 +38,29 @@ public class SchedulerService {
                 .orElseThrow(() -> new RuntimeException("El programa academnico no existe"))
                 .map(toProgramDate(startDate, endDate, pivot[0], index))
                 .collect(Collectors.toList());
+    }*/
+
+    public Flux<ProgramDate> generateCalendar(String programId, LocalDate startDate) {
+        var endDate = new AtomicReference<>(LocalDate.from(startDate));
+        final AtomicInteger[] pivot = {new AtomicInteger()};
+        final int[] index = {0};
+
+        //TODO: debe pasarlo a reactivo, no puede trabaja elementos bloqueantes
+        //TODO: trabajar el map reactivo y no deben colectar
+        // var program = programRepository.findById(programId).block();
+        var program1 = programRepository.findById(programId);
+
+        program1
+                .map(p->getDurationOf(p))
+                .map(p->toProgramDate(startDate, endDate, pivot[0], index));
+
+
+    /*
+        return Optional.ofNullable(program)
+                .map(this::getDurationOf)
+                .orElseThrow(() -> new RuntimeException("El programa academnico no existe"))
+                .map(toProgramDate(startDate, endDate, pivot[0], index))
+                .collect(Collectors.toList());*/
     }
 
     //No tocar
